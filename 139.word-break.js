4e926e9b83b62da8aa22b784bb6ee8378383dd13
sunhua <sunhua_07@163.com>
2019-03-16 23:32:13 +0800

    word-break

---
+++ 139.word-break.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=139 lang=javascript
+ *
+ * [139] Word Break
+ *
+ * https://leetcode.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (34.47%)
+ * Total Accepted:    309K
+ * Total Submissions: 895.4K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of
+ * non-empty words, determine if s can be segmented into a space-separated
+ * sequence of one or more dictionary words.
+ * 
+ * Note:
+ * 
+ * 
+ * The same word in the dictionary may be reused multiple times in the
+ * segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "leetcode", wordDict = ["leet", "code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet
+ * code".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple
+ * pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output: false
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    
+    let dp = [true]
+    for(let i = 0; i <= s.length; i++) {
+        for (let word of wordDict) {
+            if(s.substring(i-word.length, i) === word) {
+                if(dp[i-word.length]) {
+                    dp[i] = true
+                }
+            }
+        }
+    }
+
+    return !!dp[s.length]
+}
+
